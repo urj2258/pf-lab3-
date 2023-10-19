385914acb35f643a5a7f5d63c3a520e6aafccc47
urj2258 <liaqtumer296@gmail.com>
2023-10-20 00:22:53 +0500

    7. done

--- src/exercises.c
+++ src/exercises.c
@@ -80,13 +80,43 @@ int main(int argc, char **argv) {
       }
   //7. Compute the summation of even numbers 1 thru n and print
   //   the result; example (n=5): 2 + 4 = 6
+int h;
+ printf("enter a number:");
+    scanf("%d", &h);
 
+    for(int i=0;i<=h;i+=2)
+    {
+      int sum=0;
+      sum=sum+i;
+      printf("sum=%d", sum);
+    }
   //8. Write a solution to the following variation of the FizzBuzz
   //   problem.  Print out integers 1 thru n, one to a line, except
   //   that if the integer is divisible by 7 print "Foo" instead.
   //   If the integer is divisible by 11 print "Bar" instead.  If
   //   the integer is divisible by both 7 and 11, print "FooBar"
   //   instead.
+int g;
+ printf("enter a number:");
+    scanf("%d", &g);
 
+    for(int i=1;i<=g;i++)
+    {
+      if(i%7==0)
+      {
+        printf("foo\n");
+      }
+      else if(i%11==0)
+      {
+        printf("bar\n");
+      }
+      else if(i%7==0 && i%11==0)
+      {
+        printf("foo bar\n");
+      }
+      else printf("%d", i);
+
+
+    }
   return 0;
 }
